fe778af4535f488c041c0c11f826da0aebb32ae6
mananjot-17 <mananjot.singh@aimlarger.com>
2025-02-06 21:13:19 +0530

    setting up websocket server

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import { Server } from "socket.io";
+import http from "http";
 
 dotenv.config();
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173", // Frontend URL
+    methods: ["GET", "POST"],
+  },
+});
 
 app.use(cors());
 app.use(express.json());
@@ -13,8 +22,15 @@ app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
+io.on("connection", (socket) => {
+  console.log("A user connected");
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectDB();
   console.log(`Server is running on port ${PORT}`);
 });
