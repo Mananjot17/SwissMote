5bc0501503bfe835a751f0359ed7b60e26788a7b
mananjot-17 <mananjot.singh@aimlarger.com>
2025-02-09 21:13:30 +0530

    upadating url

--- backend/server.js
+++ backend/server.js
@@ -52,7 +52,7 @@ const server = http.createServer(app);
 // WebSocket setup
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://swiss-mote.vercel.app",
     credentials: true,
   },
 });
@@ -61,7 +61,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://swiss-mote.vercel.app",
     credentials: true,
   })
 );
