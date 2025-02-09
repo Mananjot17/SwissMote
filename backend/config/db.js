f43ec9f15c954fc59b0d43f886a20eb804f72a03
mananjot-17 <mananjot.singh@aimlarger.com>
2025-02-09 20:56:18 +0530

    upadating url

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(
+      "mongodb+srv://mananjotsingh17:mananjotsingh17@eventmanagementswissmot.0qcb5.mongodb.net/?retryWrites=true&w=majority&appName=eventManagementSwissmote"
+    );
     console.log("MongoDB Connected");
   } catch (err) {
     throw new Error(err);
